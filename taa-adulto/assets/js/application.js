8fbd22df31d845b7a82cb860272ec228a765e139
Marcos Silva <chuckymass@gmail.com>
2015-12-02 00:51:16 -0300

    carregando objeto a partir do centro; carregando imagem de forma sincrona e assincrona;

--- taa-adulto/assets/js/application.js
+++ taa-adulto/assets/js/application.js
@@ -2,14 +2,14 @@
     'use strict'
     var stage;
     var disposicaoObjetos = [];
-    var linha = [26,102,427,508];
-    var coluna = [66,202,522,662];
+    var linha =  [56,135,460,540]; //[26,102,427,508];
+    var coluna = [96,232,552,692]; //[66,202,522,662];
     var objetos = ['jarra.png','luva.png','torta.png'];
     function init() {
         // cria a cena para guardar os objetos na tela
         stage = new createjs.Stage("canvas");
         //Adicionando a cozinha ao cenario
-        stage.add({src : "cozinha.jpg", x: 0, y: 10});
+        stage.add({src : "cozinha.jpg", x: 0, y: 10, async: true});
         //Adicionando as posicoes finais
         for (var i = linha.length - 1; i >= 0; i--) {
             for (var j = coluna.length - 1; j >= 0; j--) {
@@ -25,7 +25,7 @@
         createjs.Ticker.addEventListener("tick", stage);
     }
     function colocaObjetoNaPorta(idObjeto, idPorta) {
-        stage.add({src : objetos[idObjeto], x: disposicaoObjetos[idPorta].x, y: disposicaoObjetos[idPorta].y, scaleX: 0.5, scaleY: 0.5},true);
+        stage.add({src : objetos[idObjeto], x: disposicaoObjetos[idPorta].x, y: disposicaoObjetos[idPorta].y, scaleX: 0.5, scaleY: 0.5, drawByCenter: true},true);
     }
     function rand(max, min){
         var min = min || 0; //Por default o minimo eh zero
--- taa-adulto/assets/js/objeto.js
+++ taa-adulto/assets/js/objeto.js
@@ -5,21 +5,25 @@ var Objeto = (function () {
         this.options = options;
         this.isDraggable = isDraggable || false;
         this.carregaObjeto();
-        if(this.isDraggable){
-            this.draggable();
-        }
     }
     Objeto.prototype.arrastaObjeto = function(evt) {
         evt.target.x = evt.stageX - evt.target.getBounds().width * evt.target.scaleX / 2;
         evt.target.y = evt.stageY - evt.target.getBounds().height * evt.target.scaleY / 2;
     };
     Objeto.prototype.carregaBitmap = function() {
-        this.bitmapObject = new createjs.Bitmap("assets/images/" + this.options.src);
-        this.bitmapObject.x = this.options.x;
-        this.bitmapObject.y = this.options.y;
-        this.bitmapObject.scaleX = this.options.scaleX || 1;
-        this.bitmapObject.scaleY = this.options.scaleY || 1;
-        this.stage.addChild(this.bitmapObject);
+        if(this.options.async){
+            this.bitmapObject = new createjs.Bitmap("assets/images/" + this.options.src);
+            carregaBMP(this);
+        }else{
+            var self = this;
+            var image = new Image();
+            image.src = "assets/images/" + self.options.src;
+            image.onload = handleImageLoad;
+            function handleImageLoad(){
+                self.bitmapObject = new createjs.Bitmap(image);
+                carregaBMP(self);
+            }
+        }
     };
     Objeto.prototype.carregaObjeto = function() {
         this.carregaBitmap(this.stage, this.options.src, this.options.x || 0, this.options.y || 0);
@@ -29,6 +33,24 @@ var Objeto = (function () {
             this.bitmapObject.addEventListener("pressmove", this.arrastaObjeto);
         }
     };
+    Objeto.prototype.drawByCenter = function(){
+        this.bitmapObject.x -= this.bitmapObject.getBounds().width * this.bitmapObject.scaleX / 2;
+        this.bitmapObject.y -= this.bitmapObject.getBounds().height * this.bitmapObject.scaleY / 2;
+    };
+    //Metodo privado da classe
+    function carregaBMP(parent){
+        parent.bitmapObject.scaleX = parent.options.scaleX || 1;
+        parent.bitmapObject.scaleY = parent.options.scaleY || 1;
+        parent.bitmapObject.x = parent.options.x;
+        parent.bitmapObject.y = parent.options.y;
+        parent.stage.addChild(parent.bitmapObject);
+        if(parent.isDraggable){
+            parent.draggable();
+        }
+        if(parent.options.drawByCenter){
+            parent.drawByCenter();
+        }
+    }
     return Objeto;
 })();
 
