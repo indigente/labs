9bf27c86db3310f75c6efe61b005af924dca8512
diegozabot <diegozabot@gmail.com>
2016-05-04 19:35:24 -0300

    added first part of user data

--- taa-adulto/assets/js/cenas.js
+++ taa-adulto/assets/js/cenas.js
@@ -13,6 +13,7 @@ function escondeTudo() {
   'use strict';
   $("#canvas").hide();
   $(".mensagem").hide();
+  $(".formulario").hide();
 }
 
 function mostraTelaEmBranco() {
@@ -29,9 +30,31 @@ function mostraMensagem(idMensagem) {
   createjs.Ticker.setPaused(true);
 }
 
+function mostraFormulario() {
+  'use strict';
+  escondeTudo();
+  $(".formulario." + idioma).show();
+  createjs.Ticker.setPaused(true);
+}
+
 function mostraJogo() {
   'use strict';
   escondeTudo();
   $("#canvas").show();
   createjs.Ticker.setPaused(false);
+}
+
+function validaForm(){
+  'use strict';
+  var valido=false;
+  if((document.getElementById('nome').value).length>0)
+    if(document.getElementById('sexo').value!="none")
+      if(document.getElementById('nascimento').value!="")
+        if(document.getElementById('escolaridade').value!="none")
+          if(document.getElementById('local').value!="none")
+            valido=true;
+  if(valido)
+    mostraJogo();
+  else
+    alert("Compilar todo o formulario!");
 }
